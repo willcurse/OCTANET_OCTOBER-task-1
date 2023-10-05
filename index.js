f4b2ebbb1d433cfa571c5d07708d1d062f80883e
utcurse <136434342+willcurse@users.noreply.github.com>
2023-10-05 14:54:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgslider(anything){
+    document.querySelector('.shoe').src=anything;
+}
+
+function colorchnge(color){
+    const circle=document.querySelector('.circle');
+    circle.style.background=color;
+}
